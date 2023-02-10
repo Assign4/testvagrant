d43b7c620495d7a5e232bc556e9171543b656d77
Assign4 <manojkumarsalugu@gmail.com>
2023-02-10 22:44:24 +0530

    Added two tests

--- cypress/e2e/ipl.spec.cy.js
+++ cypress/e2e/ipl.spec.cy.js
@@ -1,9 +1,45 @@
 describe('template spec', () => {
-  it('Test to validate 4 foreign players in a team', () => {
-    cy.visit('https://example.cypress.io')
+
+
+  const getFoeignPayerCount = (team_details) => {
+    let count = 0;
+    team_details.forEach( (player) => {
+      //console.log(typeof player.country)
+      if(player.country != 'India') {
+        count ++;
+      }
+    })
+    return count;
+  }
+
+  const getKeeperCount = (team_details) => {
+    let count = false;
+    team_details.forEach( (player) => {
+      //console.log(typeof player.country)
+      if(player.role != 'Wicket-keeper') {
+        count ++;
+      }
+    })
+    if(count > 0) {
+      count = true;
+    }
+    return count;
+  }
+
+  beforeEach(() => {
+    cy.fixture('example').then(function (example) {
+      this.example = example
   })
 
-  it('Test to validate atleast one wicket keeper', () => {
+})
+
+
+  it('Test to validate 4 foreign players in a team',function () {
+    expect(getFoeignPayerCount(this.example.player)).to.equal(4)
     
+  })
+
+  it('Test to validate atleast one wicket keeper',function () {
+    expect(getKeeperCount(this.example.player)).to.be.true
   })
 })
