0052cbc0c00c4d91f08e59061c48e7894ae6f3ae
Assign4 <manojkumarsalugu@gmail.com>
2023-02-10 21:55:34 +0530

    initial commit

---
+++ cypress/e2e/ipl.spec.cy.js
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('Test to validate 4 foreign players in a team', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Test to validate atleast one wicket keeper', () => {
+    
+  })
+})
